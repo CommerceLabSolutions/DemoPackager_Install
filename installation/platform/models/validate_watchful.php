7bceb1361e206ec362fff546113e4e150da787c8
Milos Aleksic <milos@aleksic.es>
2022-07-21 10:41:44 -0500

    Update validate_watchful.php

--- installation/platform/models/validate_watchful.php
+++ installation/platform/models/validate_watchful.php
@@ -14,15 +14,17 @@ $watchful_key = (isset($_POST["watchful_key"])) ? $_POST["watchful_key"] : '';
 $debug        = (isset($_POST["debug"])) ? $_POST["debug"] : false;
 $extension_id = 18;
 
-$domain = $_SERVER['HTTP_HOST'];
-$ip     = file_get_contents('https://api.ipify.org');
+$domain    = $_SERVER['HTTP_HOST'];
+$ip        = file_get_contents('https://api.ipify.org');
+$localhost = (gethostbyname($_SERVER['HTTP_HOST']) == '127.0.0.1') ? '1' : '0';
 
 $get_queries = "watchful_key=$watchful_key&domain=$domain&ip=$ip&extension_id=$extension_id";
 if ($debug)
 {
 	$get_queries .= '&debug=1';
 }
-$url         = "https://commercelab.solutions/index.php?option=com_ajax&plugin=validatewatchfull&format=json&$get_queries";
+
+$url = "https://commercelab.solutions/index.php?option=com_ajax&plugin=validatewatchfull&format=json&$get_queries&localhost=$localhost";
 
 $response = json_decode(file_get_contents($url), true)['data'][0];
 
@@ -37,5 +39,9 @@ if ($response['message_modal'] != '')
 	$response['message_modal'] = base64_decode($response['message_modal']);
 }
 
+if ()
+{
+	$this->container->session->get('jversion', '1.5.0');
+}
 
 echo json_encode($response);
